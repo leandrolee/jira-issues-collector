4c0a91b99cc09e73af6fdd1669a5603d7abfc4df
Leandro Romano Lee <llee@uolinc.com>
2021-05-11 19:36:10 -0300

    Setando valores no Google Sheets

--- issuesCollector.js
+++ issuesCollector.js
@@ -1,5 +1,9 @@
 import JiraApi from 'jira-client';
 import moment from 'moment-business-days';
+const { google } = require('googleapis');
+const sheets = google.sheets('v4');
+const keys = require('./keys.json');
+const sheetId = '1dRo5MTuT5DZ1oDPCT49m_-c_LhXw9T6njMe8OKcgeBA';
 
 moment.updateLocale('pt-br', {
    workingWeekdays: [1, 2, 3, 4, 5],
@@ -34,11 +38,12 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 		const done_story_statuses = process.env.npm_config_done_story_statuses.split(',');
 		const splitted_statuses = process.env.npm_config_done_task_statuses.split(',');
 		const end_date = process.env.npm_config_end_date;
-		const whole_weeks = !!process.env.npm_config_whole_weeks || false;
+		const whole_weeks = !!process.env.npm_config_whole_weeks || true;
 		const label = process.env.npm_config_label;
 		const last_weekday_with_end_date = moment(end_date).endOf('week').subtract(8, 'days');
 		const last_weekday_without_end_date = moment().endOf('week').subtract(8, 'days');
 		const num_weeks = process.env.npm_config_num_weeks || '12';
+		const num_tasks = process.env.npm_config_num_tasks || 1;
 		const boardIssues = await jira.searchJira(`project = ${project} AND (issuetype IN (Sub-Block, Sub-Imp, Sub-Bug, "Sub-A&D", "Sub-Daily and Alignments", Sub-DB, Sub-Test) AND issueFunction IN subtasksOf("project = ${project} AND status IN ('${done_story_statuses.join("', '")}')${label ? [' AND labels = ', label].join('') : ''}") OR issuetype IN (Incident, Block, "Daily and Alignments", "Suporte Negócios", "Suporte a equipes", Melhoria, Bug, "Service Request")) AND status changed during (${end_date ? (whole_weeks ? [last_weekday_with_end_date.clone().subtract(num_weeks, 'weeks').add(3, 'days').format('YYYY-MM-DD'), last_weekday_with_end_date.format('YYYY-MM-DD')].join(', ') : [moment(end_date).subtract(num_weeks, 'weeks').format('YYYY-MM-DD'), end_date].join(', ')) : (whole_weeks ? [last_weekday_without_end_date.clone().subtract(num_weeks, 'weeks').add(3, 'days').format('YYYY-MM-DD'), last_weekday_without_end_date.format('YYYY-MM-DD')].join(', ') : ['-', num_weeks, 'w, now()'].join(''))}) to ('${splitted_statuses.join("', '")}') ORDER BY Rank ASC`, {
 			startAt: 0,
 			maxResults: 1000,
@@ -125,6 +130,40 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 		console.table(allIssues);
 		console.table(allSubImpIssues);
 		console.table(sorted);
+
+		const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
+		client.authorize(function(error, tokens) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+		});
+
+		const dataRequest = {
+			spreadsheetId: sheetId,
+			valueInputOption: 'USER_ENTERED',
+			auth: client,
+			range: 'Parâmetros!C2:C3',
+			resource: {
+				values: [[moment(end_date).format('DD/MM/YYYY')], [parseInt(num_tasks)]]
+			},
+		};
+		await sheets.spreadsheets.values.update(dataRequest);
+
+		const throughput = Object.values(sorted).map(function(obj) {
+			return [obj['Sub-Imp']];
+		});
+		let initThroughputIndex = 6;
+		const throughputRequest = {
+			spreadsheetId: sheetId,
+			valueInputOption: 'USER_ENTERED',
+			auth: client,
+			range: `Parâmetros!C${initThroughputIndex}:C${initThroughputIndex + Object.keys(sorted).length - 1}`,
+			resource: {
+				values: throughput
+			},
+		};
+		await sheets.spreadsheets.values.update(throughputRequest);
 	} catch(err) {
 		console.log(err);
 	}
