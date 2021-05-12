dc6f6cceec06c4df6be5ba5d61f4c6cda3264edf
Leandro Romano Lee <llee@uolinc.com>
2021-05-12 15:45:32 -0300

    Removendo informacoes internas e pequenos ajustes

--- issuesCollector.js
+++ issuesCollector.js
@@ -2,18 +2,19 @@ import JiraApi from 'jira-client';
 import moment from 'moment-business-days';
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
+// Arquivo de chaves da conta de serviço do Google
 const keys = require('./keys.json');
-const sheetId = '1dRo5MTuT5DZ1oDPCT49m_-c_LhXw9T6njMe8OKcgeBA';
 
 moment.updateLocale('pt-br', {
    workingWeekdays: [1, 2, 3, 4, 5],
+   // TODO: Setar os feriados de forma automatizada
    holidays: ['01/01/2020', '10/04/2020', '21/04/2020', '01/05/2020', '11/06/2020', '07/09/2020', '12/10/2020', '02/11/2020', '25/12/2020', '01/01/2021', '02/04/2021', '21/04/2021', '03/06/2021', '07/09/2021', '12/10/2021', '02/11/2021', '15/11/2021'],
    holidayFormat: 'DD/MM/YYYY'
 });
 
 const jira = new JiraApi({
 	protocol: 'https',
-	host: 'jira.intranet.uol.com.br/jira',
+	host: process.env.npm_config_jira_host,
 	username: process.env.npm_config_jira_user,
 	password: process.env.npm_config_jira_pass,
 	apiVersion: '2',
@@ -21,7 +22,6 @@ const jira = new JiraApi({
 });
 
 const ONE_DAY = 24;
-
 const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 	let nonWorkingWeekdaysAndHolidaysInHours = 0;
 	for (let m = moment(startDate); m.isSameOrBefore(endDate, 'day'); m.add(1, 'days')) {
@@ -32,18 +32,23 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 	return nonWorkingWeekdaysAndHolidaysInHours;
 };
 
+// TODO: Separar em funções específicas
+// TODO: Talvez pegar as informações de end_date, label e num_tasks a partir do id da estória recebido como parâmetro, eliminando a necessidade de informar estes 3 parâmetros
 (async () => {
 	try {
 		const project = process.env.npm_config_project;
 		const done_story_statuses = process.env.npm_config_done_story_statuses.split(',');
 		const splitted_statuses = process.env.npm_config_done_task_statuses.split(',');
 		const end_date = process.env.npm_config_end_date;
+		// Flag para considerar semanas inteiras (segunda à sexta)
 		const whole_weeks = !!process.env.npm_config_whole_weeks || true;
 		const label = process.env.npm_config_label;
 		const last_weekday_with_end_date = moment(end_date).endOf('week').subtract(8, 'days');
 		const last_weekday_without_end_date = moment().endOf('week').subtract(8, 'days');
 		const num_weeks = process.env.npm_config_num_weeks || '12';
 		const num_tasks = process.env.npm_config_num_tasks || 1;
+		const sheet_id = process.env.npm_config_sheet_id;
+		// JQL executada no Jira
 		const boardIssues = await jira.searchJira(`project = ${project} AND (issuetype IN (Sub-Block, Sub-Imp, Sub-Bug, "Sub-A&D", "Sub-Daily and Alignments", Sub-DB, Sub-Test) AND issueFunction IN subtasksOf("project = ${project} AND status IN ('${done_story_statuses.join("', '")}')${label ? [' AND labels = ', label].join('') : ''}") OR issuetype IN (Incident, Block, "Daily and Alignments", "Suporte Negócios", "Suporte a equipes", Melhoria, Bug, "Service Request")) AND status changed during (${end_date ? (whole_weeks ? [last_weekday_with_end_date.clone().subtract(num_weeks, 'weeks').add(3, 'days').format('YYYY-MM-DD'), last_weekday_with_end_date.format('YYYY-MM-DD')].join(', ') : [moment(end_date).subtract(num_weeks, 'weeks').format('YYYY-MM-DD'), end_date].join(', ')) : (whole_weeks ? [last_weekday_without_end_date.clone().subtract(num_weeks, 'weeks').add(3, 'days').format('YYYY-MM-DD'), last_weekday_without_end_date.format('YYYY-MM-DD')].join(', ') : ['-', num_weeks, 'w, now()'].join(''))}) to ('${splitted_statuses.join("', '")}') ORDER BY Rank ASC`, {
 			startAt: 0,
 			maxResults: 1000,
@@ -127,10 +132,14 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
     		sorted[key] = byWeek[key];
 		});
 
+		// Todas as issues do período
 		console.table(allIssues);
+		// Todas as Sub-Imp do período
 		console.table(allSubImpIssues);
+		// Quantidade de issues agrupadas pelas semanas do período
 		console.table(sorted);
 
+		// Atualização de valores na planilha do Google
 		const client = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
 		client.authorize(function(error, tokens) {
 			if (error) {
@@ -139,8 +148,9 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 			}
 		});
 
+		// Input da data de início e quantidade de tarefas
 		const dataRequest = {
-			spreadsheetId: sheetId,
+			spreadsheetId: sheet_id,
 			valueInputOption: 'USER_ENTERED',
 			auth: client,
 			range: 'Parâmetros!C2:C3',
@@ -150,6 +160,7 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 		};
 		await sheets.spreadsheets.values.update(dataRequest);
 
+		// Input do throughput
 		const throughput = Object.values(sorted).map(function(obj) {
 			return [obj['Sub-Imp']];
 		});
@@ -157,7 +168,7 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 		const lastThroughputIndex = initThroughputIndex + Object.keys(sorted).length - 1;
 		const lastTableIndex = 36;
 		const throughputRequest = {
-			spreadsheetId: sheetId,
+			spreadsheetId: sheet_id,
 			valueInputOption: 'USER_ENTERED',
 			auth: client,
 			range: `Parâmetros!C${initThroughputIndex}:C${lastThroughputIndex}`,
@@ -167,12 +178,15 @@ const countNonWorkingWeekdaysAndHolidaysInHours = (startDate, endDate) => {
 		};
 		await sheets.spreadsheets.values.update(throughputRequest);
 
+		// Limpeza do throughput residual de execuções anteriores
 		const clearRequest = {
-			spreadsheetId: sheetId,
+			spreadsheetId: sheet_id,
 			auth: client,
 			range: `Parâmetros!C${lastThroughputIndex + 1}:C${lastTableIndex}`
 		};
 		await sheets.spreadsheets.values.clear(clearRequest);
+
+		console.log("Done!");
 	} catch(err) {
 		console.log(err);
 	}
